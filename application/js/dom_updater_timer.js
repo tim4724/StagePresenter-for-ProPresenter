952913aade32b51fccec0ce0e033e7754fe01b98
Tim Vogel <tim.vogel227@gmail.com>
2020-12-16 02:06:21 +0100

    Hide video timer for short videos and fix timezone problem

--- application/js/dom_updater_timer.js
+++ application/js/dom_updater_timer.js
@@ -1,20 +1,26 @@
 "use strict"
 
 function TimerDomUpdater() {
+	const timezoneOffsetInMinutes = new Date().getTimezoneOffset()
+	
 	const timerContainer = document.getElementById('left')
 	const timeHoursMinutes = document.getElementById('timeHoursMinutes')
 	const timeSeconds = document.getElementById('timeSeconds')
 	const videoTimer = document.getElementById('videoTimer')
 	const playlistElement = document.getElementById('playlist')
 	
+	const minimumVideoLengthForTimer = '00:01:00'
+	let lastKnownVideoTimerText = undefined
 	let removeTimeouts = {}
-
+	
 	function updateClock(seconds) {
-		// TODO: Time is off by 1 hour, maybe better use Date(milliseconds)...
-		let minutes = Math.floor(seconds / 60 % 60)
-		let hours = Math.floor(seconds / 3600 % 24)
+		// TODO: is the time correct?
+		let totalMinutes = Math.floor(seconds / 60) - timezoneOffsetInMinutes
+		
+		let hours = Math.floor(totalMinutes / 60 % 24)
+		let minutes = totalMinutes % 60
 		seconds = seconds % 60
-
+		
 		if (hours < 10) {
 			hours = '0' + hours
 		}
@@ -56,19 +62,37 @@ function TimerDomUpdater() {
 	}
 	
 	function updateVideo(uid, text) {
+		console.log('original uid', uid)
 		uid = 'videoTimer'
 
 		clearTimeout(removeTimeouts[uid])
 		if (text === '00:00:00' || text === '-00:00:00' || text === '--:--:--') {
-			if (videoTimer.innerText === '') {
+			if (videoTimer.style.display === 'none') {
 				// Do not show a timer, that stars with '00:00:00
 				return
 			}
 			removeTimeouts[uid] = setTimeout(function () {
-				videoTimer.innerText = ''
+				videoTimer.style.display = 'none'
 			}, 1200)
 		}
 		
+		if (text > lastKnownVideoTimerText) {
+			// A new timer since text is larger than before
+			// If the video length is short, do not display a timer
+			// To avoid timers for background videos.
+			// TODO: Is there a way to improve this logic?
+			if (text < minimumVideoLengthForTimer) {
+				videoTimer.style.display = 'none'
+			}
+		}
+		
+		// Always show timer if the timer has a value greater than the minimum length
+		if (text > minimumVideoLengthForTimer) {
+			videoTimer.style.display = 'inline'
+		}
+
+		lastKnownVideoTimerText = text
+
 		if (text.startsWith('00:')) {
 			text = text.substr(3)
 		} else if (text.startsWith('-00:')) {
