55460fa937648e240073f966bc1506500eda9772
Tim Vogel <tim.vogel227@gmail.com>
2020-12-30 00:32:09 +0100

    Improve browser window handling

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, BrowserView, screen, ipcMain } = require('electron')
-const { localStorage } = require('electron-browser-storage');
 
+let dummyWindow = undefined
+let waitingForDisplay = undefined
 let stageMonitorWindow = undefined
 let settingsWindow = undefined
-let settingsWindow2 = undefined
 
 // Enable live reload for Electron too
 require('electron-reload')(__dirname, {
     // Note that the path to electron may vary according to the main file
     electron: require(`${__dirname}/node_modules/electron`)
-});
+})
 
 ipcMain.on('displaySelected', (event, arg) => {
     if (stageMonitorWindow && !stageMonitorWindow.isDestroyed()) {
         stageMonitorWindow.close()
     }
 
-    localStorage.getItem('showOnDisplay').then(displayId => {
+    localStorageGet('showOnDisplay').then(displayId => {
         const display = getDisplayById(displayId)
         if (display) {
             createStageMonitorWindow(display.bounds)
@@ -48,15 +48,23 @@ function createStageMonitorWindow(bounds) {
         }
     })
     stageMonitorWindow.loadFile('application/stagemonitor.html')
-    console.log('stageMonitorWindow created')
     stageMonitorWindow.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
         event.preventDefault()
         createSettingsWindow()
+    })
+    stageMonitorWindow.on('move', function (ev) {
+        waitingForDisplay = true
+        stageMonitorWindow.close()
+        stageMonitorWindow = undefined
+        screenConfigChanged()
     })
     stageMonitorWindow.on('close', function (ev) {
         if (ev.sender === stageMonitorWindow) {
             stageMonitorWindow = undefined
         }
+    })
+    stageMonitorWindow.on('closed', function (ev) {
+        checkIfShouldQuit()
     })
 }
 
@@ -68,7 +76,7 @@ function createSettingsWindow () {
     settingsWindow = new BrowserWindow({
         backgroundColor: '#000000',
         darkTheme: true,
-        title: 'Stagemonitor',
+        title: 'Stagemonitor Settings',
         width: 1200,
         height: 800,
         fullscreen: false,
@@ -89,49 +97,99 @@ function createSettingsWindow () {
             settingsWindow = undefined
         }
     })
+    settingsWindow.on('closed', function (ev) {
+        checkIfShouldQuit()
+    })
+}
+
+let screenConfigChangedTimeout = undefined
+function screenConfigChanged() {
+    clearTimeout(screenConfigChangedTimeout)
+    screenConfigChangedTimeout = setTimeout(async () => {
+        if (settingsWindow && !settingsWindow.isDestroyed()) {
+            settingsWindow.webContents.send('updateDisplays')
+        }
+
+        if (waitingForDisplay) {
+            const displayId = await localStorageGet('showOnDisplay')
+            const display = getDisplayById(displayId)
+            if (display) {
+                waitingForDisplay = false
+                createStageMonitorWindow(display.bounds)
+            } else {
+                console.log('Still waiting for display')
+            }
+        }
+    }, 2000)
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(async () => {
-    function updateSettingsWindowDisplays() {
-        if (settingsWindow && !settingsWindow.isDestroyed()) {
-            settingsWindow.webContents.send('updateDisplays')
+    dummyWindow = new BrowserWindow({
+        show: false,
+        webPreferences: {
+            nodeIntegration: false
         }
-    }
-    screen.on('display-removed', updateSettingsWindowDisplays);
-    screen.on('display-added', updateSettingsWindowDisplays);
-    screen.on('display-metrics-changed', updateSettingsWindowDisplays);
+    })
+    dummyWindow.loadFile(__filename)
 
-    const displayId = await localStorage.getItem('showOnDisplay')
+    /*
+    // TODO: Only first launch ?
+    // TODO: Only on mac?
+    app.moveToApplicationsFolder({
+      conflictHandler: (conflictType) => {
+        if (conflictType === 'exists') {
+          return dialog.showMessageBoxSync({
+            type: 'question',
+            buttons: ['Halt Move', 'Continue Move'],
+            defaultId: 0,
+            message: 'An app of this name already exists'
+          }) === 1
+        }
+      }
+    })*/
 
-    // TODO: Wait for display, if not connected
+    screen.on('display-removed', screenConfigChanged)
+    screen.on('display-added', screenConfigChanged)
+    screen.on('display-metrics-changed', screenConfigChanged)
 
-    const display = getDisplayById(displayId)
-    if (display) {
-        createStageMonitorWindow(display.bounds)
+    const displayId = await localStorageGet('showOnDisplay')
+    if (displayId) {
+        const display = getDisplayById(displayId)
+        if (display) {
+            waitingForDisplay = false
+            createStageMonitorWindow(display.bounds)
+        } else {
+            console.log('Waiting for Display', displayId)
+            waitingForDisplay = true
+        }
     } else {
+        waitingForDisplay = false
         createSettingsWindow()
-
-        if (process.defaultApp === false) {
-            // TODO: Only first launch ?
-            // TODO: Only on mac
-            app.moveToApplicationsFolder({
-              conflictHandler: (conflictType) => {
-                if (conflictType === 'exists') {
-                  return dialog.showMessageBoxSync({
-                    type: 'question',
-                    buttons: ['Halt Move', 'Continue Move'],
-                    defaultId: 0,
-                    message: 'An app of this name already exists'
-                  }) === 1
-                }
-              }
-            })
-        }
     }
 })
+
+app.on('window-all-closed', () => {
+    if (!waitingForDisplay) {
+        app.quit()
+    }
+})
+
+function checkIfShouldQuit() {
+    if (waitingForDisplay) {
+        return
+    }
+    const wins = BrowserWindow.getAllWindows()
+    if (wins.length === 0 || wins.length === 1 && wins[0] === dummyWindow) {
+        app.quit()
+    }
+}
+
+function localStorageGet(key) {
+    return dummyWindow.webContents.executeJavaScript('localStorage.' + key);
+}
 
 function getDisplayById(id) {
     // Do not use '===' !
